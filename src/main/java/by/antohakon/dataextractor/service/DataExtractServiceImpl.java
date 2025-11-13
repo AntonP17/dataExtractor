002d4291026a1a6c43579e45a810cf09b7a15dcf
AntonP17 <pokrovskii.anton@mail.ru>
2025-11-13 22:53:05 +0300

    next step impl sort, find number

--- src/main/java/by/antohakon/dataextractor/service/DataExtractServiceImpl.java
+++ src/main/java/by/antohakon/dataextractor/service/DataExtractServiceImpl.java
@@ -4,11 +4,16 @@ import by.antohakon.dataextractor.dto.DataResponseDto;
 import by.antohakon.dataextractor.dto.ExtractDataDto;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Random;
 
 @Service
 @Slf4j
@@ -22,7 +27,7 @@ public class DataExtractServiceImpl implements DataExtractService {
 
         int[] numbersFromFile = extractDataFromFile(extractDataDto);
         int[] sortedNumbers = sortArray(numbersFromFile);
-        DataResponseDto dataResponseDto = findNumber(sortedNumbers[0]);
+        DataResponseDto dataResponseDto = findNumber(extractDataDto.numberPosition());
 
         return dataResponseDto;
 
@@ -48,24 +53,55 @@ public class DataExtractServiceImpl implements DataExtractService {
         }
 
         log.info("file exists :)");
+        int[] numbers = null;
 
-        int[] numbersFromFile = new int[]{1, 2, 3};
-        return numbersFromFile;
+        try (FileInputStream fis = new FileInputStream(extractDataDto.path())) {
+            Workbook workbook = new XSSFWorkbook(fis);
+            Sheet sheet = workbook.getSheetAt(0); // первый лист
+
+            // Определяем количество строк
+            int rowCount = sheet.getPhysicalNumberOfRows();
+            numbers = new int[rowCount];
+
+            // Читаем данные из столбца
+            for (int i = 0; i < rowCount; i++) {
+                Row row = sheet.getRow(i);
+                Cell cell = row.getCell(0); // первый столбец
+
+                if (cell.getCellType() == CellType.NUMERIC) {
+                    numbers[i] = (int) cell.getNumericCellValue();
+                } else {
+                    numbers[i] = 0;
+                }
+            }
+
+            // Выводим числа в консоль
+            for (int number : numbers) {
+                System.out.println("Число: " + number);
+            }
+        } catch (IOException e) {
+            log.error("файл по данному пути не найден :(");
+        }
+
+        return numbers;
 
     }
 
+    // TO DO !!!!!!!!!!!
     @Override
     public int[] sortArray(int[] array) {
         log.info("method sortArray begin");
         return array;
     }
 
+    // TO DO !!!!!!!!!!!
     @Override
     public DataResponseDto findNumber(int numberPosition) {
 
         log.info("method findNumber begin");
+        int random = new Random().nextInt(20);
         DataResponseDto dataResponseDto = DataResponseDto.builder()
-                .findNumber(3)
+                .findNumber(random)
                 .text("найденное число по запросу")
                 .build();
 
